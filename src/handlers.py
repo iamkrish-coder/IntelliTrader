8e87f01ae21db281e869cf5f26336b241a3fba05
Srikrishnan P <krishnan.sri92@gmail.com>
2024-01-14 16:59:18 +0530

    feat: modified strategy code to add base strategy class and abstract methods

--- src/handlers.py
+++ src/handlers.py
@@ -1,24 +1,18 @@
 import datetime
 
-class TickerHandler:
-    def __init__(self, fetch, ticker):
-        self.fetch = fetch
-        self.ticker = ticker
-
-    def handle_ticker(self, exchange, symbol, mode, user_settings):
-        itoken = self.fetch.stream_instrument_token_lookup(exchange, symbol)
-        self.ticker.connect_to_ticker(itoken, mode, user_settings)
-
-
-class DataHandler:
-    def __init__(self, fetch):
-        self.fetch = fetch
+class Handler:
+    def __init__(self, modules):
+        self.modules = modules
 
     def fetch_ltp(self, exchange, symbol):
-        result_ltp = self.fetch.fetch_ltp(exchange, symbol)
+        result_ltp = self.modules['fetch'].fetch_ltp(exchange, symbol)
         key = exchange + ':' + symbol
         ltp = result_ltp[key]['last_price']
         print("\nThe Last Traded Price (LTP) of {}:{} is {}\n".format(exchange, symbol, ltp))
+
+    def fetch_ticker_data(self, exchange, symbol, mode, user_settings):
+        itoken = self.modules['fetch'].stream_instrument_token_lookup(exchange, symbol)
+        self.modules['ticker'].connect_to_ticker(itoken, mode, user_settings)
 
     def fetch_backtest_data(self, exchange, symbol, interval, duration):
         user_input_for_backtest_data = {
@@ -29,5 +23,5 @@ class DataHandler:
             'start_date': datetime.date(2023, 12, 29),
             'end_date': datetime.date(2023, 12, 31)
         }
-        datasource = self.fetch.fetch_backtest_data(user_input_for_backtest_data)
+        datasource = self.modules['fetch'].fetch_backtest_data(user_input_for_backtest_data)
         print("\nThe OHLC values for {}:{} on {} timeframe: \n{}".format(exchange, symbol, interval, datasource))
---
+++ src/strategies/options_strategy.py
@@ -0,0 +1,41 @@
+# strategies/options_strategy.py
+from .base_strategy import BaseStrategy
+import strategies.shared as shared
+
+class OptionsStrategy(BaseStrategy):
+    def __init__(self, connection):
+        super().__init__(connection)
+
+    def execute_strategy(self, dataset):
+        if dataset:
+            common_utils = dataset['common_utils']
+            user_input = dataset['user_input']
+
+            # Input values
+            m_ticker = user_input['ticker']
+            m_strike = user_input['strike']
+            m_exchange = user_input['exchange']
+            m_expiry_offset = user_input['expiry']
+            m_strike_offset = user_input['strikes']
+
+            # Get derivative instrument's last traded price
+            current_index = shared.get_underlying_ltp(self.connection, m_ticker)
+
+            if current_index:
+                # Get NFO instruments dump
+                nfo_dump = common_utils['fetch'].fetch_instruments(m_exchange)
+
+                # Get specific instrument's option contracts
+                ce_options = shared.get_options(nfo_dump, m_ticker, m_strike[0], m_exchange)
+                pe_options = shared.get_options(nfo_dump, m_ticker, m_strike[1], m_exchange)
+
+                # Get specific instrument's current expiry option contracts
+                ce_options_current = shared.get_options_with_expiry(ce_options, m_expiry_offset)
+                pe_options_current = shared.get_options_with_expiry(pe_options, m_expiry_offset)
+
+                # Get option chain for call and put contracts
+                call_option_chain = shared.get_call_option_chain(ce_options_current, current_index, m_strike_offset)
+                put_option_chain = shared.get_put_option_chain(pe_options_current, current_index, m_strike_offset)
+
+                print(call_option_chain)
+                print(put_option_chain)
