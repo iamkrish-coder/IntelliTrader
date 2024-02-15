b6ac7422a2475ed17b12dffd646eec83088e1ccc
Srikrishnan P <krishnan.sri92@gmail.com>
2024-02-15 07:58:19 +0530

    fix: Remove handlers

--- src/handlers/virtual_strategy_handler - Copy.py
+++
@@ -1,9 +0,0 @@
-from turtle import st
-from src.constants.constants import *
-from src.enumerations.enums import *
-import pandas as pd
-
-class VirtualStrategyHandler:
-    def __init__(self):
-        pass
-        
