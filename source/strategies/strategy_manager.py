52d39d958d5cb624397cbec0360065ce23ac8d09
Srikrishnan P <krishnan.sri92@gmail.com>
2024-02-20 07:47:56 +0530

    feat!: Add landing page

--- source/strategies/strategy_manager.py
+++ source/strategies/strategy_manager.py
@@ -11,7 +11,7 @@ class StrategyManager:
     def initialize_strategy(self, configuration):
         try:
             live_trade = configuration.get("live_trade").lower()
-            auto_virtual = configuration.get("virtual").lower()
+            virtual_trade = configuration.get("virtual_trade").lower()
             
             strategy_id = int(configuration.get("strategy"))
             strategy_enum = Strategy(strategy_id)
@@ -27,14 +27,25 @@ class StrategyManager:
                 # Instantiate the strategy with connection and parameters
                 strategy_instance = strategy_class(self.connection, self.modules, **strategy_params)
                 
-                # Execute the strategy
+                # Strategy Execution
                 if live_trade == 'true':
-                    strategy_instance.execute_live_strategy()                  # Live Trade
-                elif auto_virtual == 'true' and live_trade == 'false':
-                   auto_params = configuration.get("virtual_params")           # Virtual Trade
-                   strategy_instance.execute_virtual_strategy(auto_params)               
+                    # Load Default Configs
+                    live_trade_params = configuration.get("virtual_trade_params")              
+                    market_params = configuration.get("marktet_params")
+                    
+                    # Instantiate Strategy Method
+                    strategy_instance.execute_live_strategy(live_trade_params, market_params)
+                    
+                elif virtual_trade == 'true' and live_trade == 'false':
+                    # Load Default Configs
+                    virtual_trade_params = configuration.get("virtual_trade_params")              
+                    market_params = configuration.get("marktet_params")
+                    
+                    # Instantiate Strategy Method
+                    strategy_instance.execute_virtual_strategy(virtual_trade_params, market_params)      
+                    
                 else:
-                    logging.info("No auto trade or virtual configuration found.")     # No Trade
+                    logging.info("No Virtual trade or Live configuration found.")       
             else:
                 logging.info(f"Strategy class {strategy_class_name} not found in the strategies module.")
 
