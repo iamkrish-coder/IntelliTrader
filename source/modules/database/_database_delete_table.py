b0e6a97d88b7cf01b592fd7d9520154d0602e972
Srikrishnan P <krishnan.sri92@gmail.com>
2024-03-29 15:17:04 +0530

    fix: integrate boto3 into delete table module

--- source/modules/database/_database_delete_table.py
+++ source/modules/database/_database_delete_table.py
@@ -4,8 +4,11 @@ from source.constants.constants import *
 from source.enumerations.enums import *
 from source.utils.logging_utils import *
 from source.modules.database.comparison_operator import ComparisonOperators
-from source.aws.DynamoDB.aws_dynamodb import AWSDynamoDB
-from botocore.client import ClientError
+
+from boto3.exceptions import botocore
+from botocore.exceptions import ClientError
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
 
 class DatabaseDeleteTable:
     def __init__(self, configuration, table_name):
@@ -16,10 +19,20 @@ class DatabaseDeleteTable:
         return self.delete_table()
     
     def delete_table(self):      
-        # Delete a Table
-        if self.table_name is None:
-            return False
-        
-        dynamodb_resource = AWSDynamoDB(self.table_name)
-        dynamodb_resource.delete_table()
+        response = None
+        if self.table_name is not None:
+            try:
+                log_info(f"Deleting table '{self.table_name}'...")
+                response = self.table.delete()
+                if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+                    log_info(f"Delete '{self.table_name}' Table ...COMPLETE!")
+            except ClientError as e:
+                log_error(
+                    "Couldn't delete table. Here's why: %s: %s",
+                    e.response["Error"]["Code"],
+                    e.response["Error"]["Message"]
+                )
+                raise
+
+        return response
         
