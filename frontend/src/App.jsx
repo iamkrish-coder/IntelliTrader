004d800a39069880879bc9ec679a33c306582bee
Srikrishnan <krishnan.sri92@gmail.com>
2024-10-14 10:28:45 +0530

    fix: improved landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import './assets/scss/style.scss';
 import Home from './pages/Home'
 import PageNotFound from './pages/PageNotFound';
 import ApplicationRoutes from './router/ApplicationRoutes.jsx';
 import IMAGES from './constants/Images.js';
 import ICONS from './constants/Icons.js';
+import './assets/scss/custom-style.scss';
 
 const constants = {
     IMAGES : IMAGES,
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import { useNavigate } from 'react-router-dom'
 import { Button, Container } from 'react-bootstrap';
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const Home = ({ assets }) => {
-    // Props Destructuring
     const { IMAGES, ICONS } = assets; 
     const navigate = useNavigate()
+
+    const [heading] = useTypewriter({
+        words: ['IntelliTrader'],
+        loop: 3,
+        typeSpeed: 100,
+        deleteSpeed: 50,
+        delaySpeed: 3000
+      })
 
     return (
         <Container fluid className="home-container">
@@ -12,9 +20,9 @@ const Home = ({ assets }) => {
                 <div className="home-overlay"></div>
             </div>
             <div className="home-content">
-                <h1 className="home-title ">IntelliTrader</h1>
-                <Button variant="primary" onClick={() => navigate('/login')}>
-                    Get Started
+                <h1 className="home-title m-2">{heading}<Cursor cursorColor="red" cursorStyle='_' /></h1>
+                <Button variant="primary" size="sm" className="m-2 px-2" onClick={() => navigate('/login')}>
+                    <span>Get started</span>
                 </Button>
             </div>
         </Container>
